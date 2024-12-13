5b68fa8921e4ad530f7971b9182240cc908a8f96
Rinkaj-Solanki <rinkaj909@gmail.com>
2024-12-13 22:27:57 +0530

    creating ec2 instance from terraform

---
+++ tf-project/aws-ec2/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.54.1"
+    }
+  }
+}
+
+
+
+provider "aws" {
+  # configuration options
+  region = "us-east-1"
+}
+
+resource "aws_instance" "ec2server" {
+  ami = "ami-0453ec754f44f9a4a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Sample EC2 server"
+  }
+}
