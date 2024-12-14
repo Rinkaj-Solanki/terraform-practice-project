874c9225312a676788bfb1eafa917b1b1451fdc0
Rinkaj-Solanki <rinkaj909@gmail.com>
2024-12-14 23:57:42 +0530

    created internet gateway, route table with route rule & associated the aws route table to the public subnet

--- tf-project/aws-vpc/main.tf
+++ tf-project/aws-vpc/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.54.1"
     }
   }
@@ -20,7 +20,7 @@ resource "aws_vpc" "my-tf-vpc" {
 
 resource "aws_subnet" "private-subnet" {
   cidr_block = "10.0.1.0/24"
-  vpc_id = aws_vpc.my-tf-vpc.id
+  vpc_id     = aws_vpc.my-tf-vpc.id
   tags = {
     Name = "tf-private-subnet"
   }
@@ -28,9 +28,40 @@ resource "aws_subnet" "private-subnet" {
 
 resource "aws_subnet" "public-subnet" {
   cidr_block = "10.0.2.0/24"
-  vpc_id = aws_vpc.my-tf-vpc.id
+  vpc_id     = aws_vpc.my-tf-vpc.id
   tags = {
     Name = "tf-public-subnet"
   }
 }
+
+resource "aws_internet_gateway" "internet-gateway" {
+  vpc_id = aws_vpc.my-tf-vpc.id
+  tags = {
+    Name = "tf-internet-gateway"
+  }
+}
+
+resource "aws_route_table" "route-table" {
+  vpc_id = aws_vpc.my-tf-vpc.id
+
+  # routing rule
+  route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.internet-gateway.id
+    }
+}
+
+resource "aws_route_table_association" "public-sub" {
+  subnet_id      = aws_subnet.public-subnet.id
+  route_table_id = aws_route_table.route-table.id
+}
+
+
+
+
+
+
+
+
+
 
