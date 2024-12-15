9435441f6af17b4656c5651333e1581cce07fe31
Rinkaj-Solanki <rinkaj909@gmail.com>
2024-12-15 12:53:46 +0530

    created security group and attached to the ec2 server and connected nginx to ec2

--- tf-project/aws-ec2-vpc-nginx/ec2.tf
+++ tf-project/aws-ec2-vpc-nginx/ec2.tf
@@ -2,6 +2,14 @@ resource "aws_instance" "nginx_server" {
   ami           = "ami-0453ec754f44f9a4a"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [aws_security_group.nginx_sg.id]
+  associate_public_ip_address = true
+
+  user_data = <<-EOF
+            #!/bin/bash
+            sudo yum install nginx -y
+            sudo systemctl start nginx
+            EOF
 
   tags = {
     Name = "NginxServer"
