a7d7fd687a10a061078d9f63804f667381e8f9d1
Rinkaj-Solanki <rinkaj909@gmail.com>
2024-12-15 13:18:46 +0530

    changed ami

--- tf-project/aws-ec2-vpc-nginx/ec2.tf
+++ tf-project/aws-ec2-vpc-nginx/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "nginx_server" {
-  ami           = "ami-0453ec754f44f9a4a"
+  ami           = "ami-001328f6d77eb9274"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.nginx_sg.id]
